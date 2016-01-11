3a46ff7e8548dd24496ca5147770eca73584f4c0
Billy Rangel <billy@billy.local>
2016-01-11 15:36:42 -0600

    .

--- testGit/ViewController.swift
+++ testGit/ViewController.swift
@@ -10,6 +10,9 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var label: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -19,7 +22,11 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
+    
+    @IBAction func Saludo(sender: AnyObject) {
+        label.text = "Hola Swift";
+    }
 
 }
 
