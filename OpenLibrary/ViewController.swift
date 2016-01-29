277289f29030daf758220c227e1e808b38ad67d7
Billy Rangel <billy@billy.lan>
2016-01-29 13:37:53 -0600

    .

--- OpenLibrary/ViewController.swift
+++ OpenLibrary/ViewController.swift
@@ -12,45 +12,133 @@ class ViewController: UIViewController, UITextFieldDelegate {
     
     
     @IBOutlet weak var textoBusqueda: UITextField!
-    @IBOutlet weak var resultadoJSON: UITextView!
+    @IBOutlet weak var titulo: UILabel!
+    @IBOutlet weak var autores: UILabel!
+    @IBOutlet weak var portada: UIImageView!
     
     
     override func viewDidLoad() {
+        
         self.textoBusqueda.delegate = self
+        
+        self.titulo.text = ""
+        self.autores.text = ""
+        
     }
     
+    
     func textFieldShouldReturn(textField: UITextField) -> Bool {
+        
         if textField == textoBusqueda {
+            
             textField.resignFirstResponder()
-            return false
+            
+            
+            if self.textoBusqueda.text == "" {
+                self.alerta("Falta ingresar el ISBN del libro")
+                return false
+            } else {
+                self.titulo.text = ""
+                self.autores.text = ""
+                self.portada.image = nil
+                Buscar()
+            }
         }
         return true
     }
+    
+    override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
+        self.view.endEditing(true)
+    }
 
 
-    @IBAction func Buscar(sender: AnyObject) {
+    func Buscar() {
+        
         var urls = "https://openlibrary.org/api/books?jscmd=data&format=json&bibkeys=ISBN:"
         urls.appendContentsOf(textoBusqueda.text!)
+        
         let url = NSURL(string: urls)
         let sesion = NSURLSession.sharedSession()
         
-        let bloque = { (datos:NSData?, resp:NSURLResponse?, error:NSError?) -> Void in
-        
-            // Validar errores
-            let http = resp as! NSHTTPURLResponse
-            if http.statusCode < 200 || http.statusCode >= 300 {
-                print("ERROR: \(http.description)")
-                return
+        let callback = { (datos:NSData?, response:NSURLResponse?, error:NSError?) -> Void in
+            
+            if response != nil {
+                
+                dispatch_async(dispatch_get_main_queue(), {
+                    //let json = NSString(data: datos!, encoding: NSUTF8StringEncoding)
+                    
+                    do {
+                        
+                        let json = try NSJSONSerialization.JSONObjectWithData(datos!, options: NSJSONReadingOptions.MutableLeaves)
+                        let clave = "ISBN:" + self.textoBusqueda.text!
+                        
+                        if let info = json[clave] as? NSDictionary {
+                            
+                            // Titutlo
+                            if let titulo = info["title"] as? String {
+                                self.titulo.text = titulo
+                            }
+                            
+                            
+                            // Autores
+                            if let autoresLista = info["authors"] as? NSArray {
+                                var buffer = ""
+                                
+                                for autor in autoresLista {
+                                    buffer.appendContentsOf( String(format: "%@\n", (autor["name"] as? String)! ) )
+                                }
+                                self.autores.text = buffer
+                            }
+                            
+                            
+                            // Imagen de portada
+                            if let imagen = info["cover"] as? NSDictionary {
+                                let url = NSURL( string: imagen["medium"] as! String )
+                                let data = NSData(contentsOfURL: url!)
+                                self.portada.image = UIImage(data: data!)
+                            }
+                            
+                            
+                        }
+                        
+                        
+                    }
+                    catch _ {
+                    }
+                    
+                })
+            } else {
+                self.alerta("ERROR: Falla con Internet")
             }
             
-            dispatch_async(dispatch_get_main_queue()) { [unowned self] in
-                let texto = NSString(data: datos!, encoding: NSUTF8StringEncoding)
-                self.resultadoJSON.text = texto! as String
-            }
         }
         
-        let dt = sesion.dataTaskWithURL(url!, completionHandler: bloque)
+        
+        let dt = sesion.dataTaskWithURL(url!, completionHandler: callback)
         dt.resume()
     }
-}
+    
+    
+    
+    
+    func alerta(msg:String) {
+        let alerta = UIAlertController(title: "OpenLibrary", message: msg, preferredStyle: .Alert)
+        alerta.addAction(UIAlertAction(title: "Aceptar", style: .Default, handler: nil))
+        self.presentViewController(alerta, animated: true, completion: nil)
+    }
+    
+    
+        
+} // End Clase
+
+
+
+
+
+
+
+
+
+
+
 
